7e60a476b2c98608efbb428ad00de634d804bef1
bmourad01 <Benjamin_Mourad@student.uml.edu>
2022-12-31 20:04:38 -0500

    Fix typos in docs

--- src/lib/virtual.mli
+++ src/lib/virtual.mli
@@ -277,7 +277,7 @@ module Insn : sig
 
     (** Creates a switch table from an association list.Afl_instrument
 
-        @raise [Invalid_argument] the list has duplicate keys.
+        @raise [Invalid_argument] if the list has duplicate keys.
     *)
     val table : (Bitvec.t * Label.t) list -> table
 
@@ -454,7 +454,7 @@ module Blk : sig
   (** Pretty prints a basic block. *)
   val pp : Format.formatter -> t -> unit
 
-  (** Pretty.prints a basic block, where instructions are indented and
+  (** Pretty prints a basic block, where instructions are indented and
       unlabeled. *)
   val pp_hum : Format.formatter -> t -> unit
 end
