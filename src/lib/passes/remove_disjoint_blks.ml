71875e1a7650a6a11abcf388c65645cd4d21a5b0
bmourad01 <Benjamin_Mourad@student.uml.edu>
2023-06-04 23:37:46 -0400

    Fix use of DFS in `Remove_disjoint_blks`
    The generic `depth_first_search` function in Graphlib will explore
    every tree, not just the one spanning from our starting node. So we
    should terminate the search early.

--- src/lib/passes/remove_disjoint_blks.ml
+++ src/lib/passes/remove_disjoint_blks.ml
@@ -3,12 +3,19 @@ open Graphlib.Std
 open Regular.Std
 open Virtual
 
+exception Done of Label.Set.t
+
+let dfs cfg start =
+  Graphlib.depth_first_search (module Cfg) cfg ~start
+    ~init:Label.Set.empty
+    ~start_tree:(fun n s ->
+        if Label.(n = start) then s else raise @@ Done s)
+    ~enter_node:(fun _ n s -> Set.add s n)
+
 let reachable fn =
   let cfg = Cfg.create fn in
-  Graphlib.depth_first_search (module Cfg) cfg
-    ~start:(Func.entry fn)
-    ~init:Label.Set.empty
-    ~enter_node:(fun _ n s -> Set.add s n)
+  let start = Func.entry fn in
+  try dfs cfg start with Done s -> s
 
 let run fn =
   Func.blks fn |> Seq.map ~f:Blk.label |>
