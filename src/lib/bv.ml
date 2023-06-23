3a58e57e668bfab8dbabc130a205326b006e4eac
bmourad01 <Benjamin_Mourad@student.uml.edu>
2023-06-23 11:39:20 -0400

    Cleanup `Bv`

--- src/lib/bv.ml
+++ src/lib/bv.ml
@@ -2,17 +2,18 @@
 
     For more documentation, see:
 
-    {:https://binaryanalysisplatform.github.io/bap/api/master/bitvec/Bitvec/index.html }
-    {:https://binaryanalysisplatform.github.io/bap/api/master/bitvec-binprot/Bitvec_binprot/index.html }
+    {:https://binaryanalysisplatform.github.io/bap/api/master/bitvec/Bitvec/index.html}
+    {:https://binaryanalysisplatform.github.io/bap/api/master/bitvec-binprot/Bitvec_binprot/index.html}
     {:https://binaryanalysisplatform.github.io/bap/api/master/bitvec-sexp/Bitvec_sexp/index.html}
     {:https://binaryanalysisplatform.github.io/bap/api/master/bitvec-order/Bitvec_order/index.html}
 *)
-
-open Core
 
 include Bitvec
 include Bitvec_binprot
 include Bitvec_order
 include Bitvec_sexp
 
-let hash_fold_t st t = Hash.fold_string st @@ to_string t
+include Core.Hashable.Make_and_derive_hash_fold_t(struct
+    include Bitvec
+    include Bitvec_sexp
+  end)
