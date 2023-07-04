60941d6a74a7d70066604f2327811e9b2dc3d9ce
bmourad01 <Benjamin_Mourad@student.uml.edu>
2023-07-04 14:46:02 -0400

    Rename to `Peephole`

--- src/lib/passes/passes.ml
+++ src/lib/passes/passes.ml
@@ -1,4 +1,4 @@
-module Prop_consts = Prop_consts
+module Peephole = Peephole
 module Remove_dead_vars = Remove_dead_vars
 module Remove_disjoint_blks = Remove_disjoint_blks
 module Ssa = Ssa
--- src/lib/passes/peephole.ml
+++ src/lib/passes/peephole.ml
@@ -4,8 +4,15 @@ open Context.Syntax
 let rules =
   let open Egraph.Enode in
   let open Egraph.Rule in
-  let x = var "x" in [
-    (Ounop (`copy `i8)  & [x]) => x;
+  let x = var "x" in
+  let y = var "y" in [
+    (* Unconditional branch. *)
+    (Obr & [exp (Obool  true); x; y]) => (Ojmp & [x]);
+    (Obr & [exp (Obool false); x; y]) => (Ojmp & [y]);
+    (* Branch to same destination. *)
+    (Obr & [x; y; y]) => (Ojmp & [y]);
+    (* Copy propagation. *)
+    (Ounop (`copy  `i8) & [x]) => x;
     (Ounop (`copy `i16) & [x]) => x;
     (Ounop (`copy `i32) & [x]) => x;
     (Ounop (`copy `i64) & [x]) => x;
