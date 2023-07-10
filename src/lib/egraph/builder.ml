6d9e4563fcc34cfe61010aa09db2924b852e59ab
bmourad01 <Benjamin_Mourad@student.uml.edu>
2023-07-10 15:21:04 -0400

    Simplify worklist
    It doesn't really need to track the labels of dependencies

--- src/lib/egraph/builder.ml
+++ src/lib/egraph/builder.ml
@@ -21,25 +21,7 @@ let init () = {
   filled = Var.Table.create ();
 }
 
-(* The worklist keeps track of the dependencies that the algorithm
-   should attempt to resolve. *)
-module Worklist = struct
-  type t = Label.Set.t Var.Map.t
-
-  let empty = Var.Map.empty
-  let is_empty = Map.is_empty
-  let remove = Map.remove
-  let mem = Map.mem
-
-  let singleton x l =
-    Var.Map.singleton x @@ Label.Set.singleton l
-
-  let add w x l = Map.update w x ~f:(function
-      | None -> Label.Set.singleton l
-      | Some s -> Set.add s l)
-end
-
-type work = Worklist.t
+type work = Var.Set.t
 
 (* Lift our tree representation into e-nodes.
 
@@ -54,23 +36,23 @@ module Lifter = struct
 
   let rec pure ?(vs = Var.Set.empty) t eg (p : Exp.pure) : id =
     let pure = pure t eg ~vs in match p with
-      | Palloc (a, n) -> prov eg a (Oalloc n) []
-      | Pbinop (a, b, l, r) -> prov eg a (Obinop b) [pure l; pure r]
-      | Pbool b -> add_enode eg @@ N (Obool b, [])
-      | Pcall (a, ty, f, args, vargs) ->
-        let f = global t eg f ~vs in
-        let args = add_enode eg @@ N (Ocallargs, args' t eg args ~vs) in
-        let vargs = add_enode eg @@ N (Ocallargs, args' t eg vargs ~vs) in
-        prov eg a (Ocall ty) [f; args; vargs]
-      | Pdouble d -> add_enode eg @@ N (Odouble d, [])
-      | Pint (i, ty) -> add_enode eg @@ N (Oint (i, ty), [])
-      | Pload (a, ty, x) -> prov eg a (Oload ty) [pure x]
-      | Psel (a, ty, c, y, n) -> prov eg a (Osel ty) [pure c; pure y; pure n]
-      | Psingle s -> add_enode eg @@ N (Osingle s, [])
-      | Psym (s, o) -> add_enode eg @@ N (Osym (s, o), [])
-      | Punop (a, u, x) -> prov eg a (Ounop u) [pure x]
-      | Pvar x when Set.mem vs x -> raise @@ Occurs_failed (x, None)
-      | Pvar x -> var t eg vs x
+    | Palloc (a, n) -> prov eg a (Oalloc n) []
+    | Pbinop (a, b, l, r) -> prov eg a (Obinop b) [pure l; pure r]
+    | Pbool b -> add_enode eg @@ N (Obool b, [])
+    | Pcall (a, ty, f, args, vargs) ->
+      let f = global t eg f ~vs in
+      let args = add_enode eg @@ N (Ocallargs, args' t eg args ~vs) in
+      let vargs = add_enode eg @@ N (Ocallargs, args' t eg vargs ~vs) in
+      prov eg a (Ocall ty) [f; args; vargs]
+    | Pdouble d -> add_enode eg @@ N (Odouble d, [])
+    | Pint (i, ty) -> add_enode eg @@ N (Oint (i, ty), [])
+    | Pload (a, ty, x) -> prov eg a (Oload ty) [pure x]
+    | Psel (a, ty, c, y, n) -> prov eg a (Osel ty) [pure c; pure y; pure n]
+    | Psingle s -> add_enode eg @@ N (Osingle s, [])
+    | Psym (s, o) -> add_enode eg @@ N (Osym (s, o), [])
+    | Punop (a, u, x) -> prov eg a (Ounop u) [pure x]
+    | Pvar x when Set.mem vs x -> raise @@ Occurs_failed (x, None)
+    | Pvar x -> var t eg vs x
 
   and var t eg vs x = match Hashtbl.find t.pure x with
     | None -> add_enode eg @@ N (Ovar x, [])
@@ -114,42 +96,42 @@ module Lifter = struct
         add_enode eg @@ N (Osw ty, pure i :: local t eg d :: tbl)
 end
 
-let operand (o : operand) w l : Exp.pure * work = match o with
+let operand (o : operand) w : Exp.pure * work = match o with
   | `bool f     -> Pbool f, w
   | `int (i, t) -> Pint (i, t), w
   | `float f    -> Psingle f, w
   | `double d   -> Pdouble d, w
   | `sym (s, o) -> Psym (s, o), w
-  | `var v      -> Pvar v, Worklist.add w v l
+  | `var v      -> Pvar v, Set.add w v
 
-let operands os w l =
+let operands os w =
   List.fold_right os ~init:([], w) ~f:(fun o (os, w) ->
-      let o, w = operand o w l in
+      let o, w = operand o w in
       o :: os, w)
 
-let global (g : Virtual.global) w l : Exp.global * work = match g with
+let global (g : Virtual.global) w : Exp.global * work = match g with
   | `addr a -> Gaddr a, w
   | `sym s  -> Gsym s, w
-  | `var v  -> Gpure (Pvar v), Worklist.add w v l
+  | `var v  -> Gpure (Pvar v), Set.add w v
 
-let local (loc : Virtual.local) w l = match loc with
+let local (loc : Virtual.local) w = match loc with
   | `label (lbl, args) ->
-    let args, w = operands args w l in
+    let args, w = operands args w in
     (lbl, args), w
 
-let dst (d : Virtual.dst) w l : Exp.dst * work = match d with
+let dst (d : Virtual.dst) w : Exp.dst * work = match d with
   | #Virtual.global as g ->
-    let g, w = global g w l in
+    let g, w = global g w in
     Dglobal g, w
   | #Virtual.local as loc ->
-    let loc, w = local loc w l in
+    let loc, w = local loc w in
     Dlocal loc, w
 
-let table tbl w l =
+let table tbl w =
   let tbl, w =
     Ctrl.Table.enum tbl |>
     Seq.fold ~init:([], w) ~f:(fun (tbl, w) (i, loc) ->
-        let loc, w = local loc w l in
+        let loc, w = local loc w in
         (i, loc) :: tbl, w) in
   List.rev tbl, w
 
@@ -157,13 +139,13 @@ let update t l ((w, xs) as acc) x f =
   (* Fail if we've already seen this variable. *)
   if Set.mem xs x then
     raise @@ Occurs_failed (x, Some l);
-  if Worklist.mem w x then
-    let w' = ref @@ Worklist.remove w x in
+  if Set.mem w x then
+    let w' = ref @@ Set.remove w x in
     (* Assume that the current bound expression is the same. *)
     if not @@ Hashtbl.mem t.pure x then begin
       let w, p = f !w' in
       (* Fail early if we re-introduce the same variable. *)
-      if Worklist.mem w x then
+      if Set.mem w x then
         raise @@ Occurs_failed (x, Some l);
       Hashtbl.set t.pure ~key:x ~data:p;
       w' := w;
@@ -177,36 +159,36 @@ let accum t acc i =
   let go = update t l acc in
   match Insn.op i with
   | `bop (x, o, a, b) -> go x @@ fun w ->
-    let a, w = operand a w l in
-    let b, w = operand b w l in
+    let a, w = operand a w in
+    let b, w = operand b w in
     w, Pbinop (Some l, o, a, b)
   | `uop (x, o, a) -> go x @@ fun w ->
-    let a, w = operand a w l in
+    let a, w = operand a w in
     w, Punop (Some l, o, a)
   | `sel (x, t, c, y, n) -> go x @@ fun w ->
-    let c, w = Exp.Pvar c, Worklist.add w c l in
-    let y, w = operand y w l in
-    let n, w = operand n w l in
+    let c, w = Exp.Pvar c, Set.add w c in
+    let y, w = operand y w in
+    let n, w = operand n w in
     w, Psel (Some l, t, c, y, n)
   | `call (None, _, _, _) -> acc
   | `call (Some (x, t), f, args, vargs) -> go x @@ fun w ->
-    let f, w = global f w l in
-    let args, w = operands args w l in
-    let vargs, w = operands vargs w l in
+    let f, w = global f w in
+    let args, w = operands args w in
+    let vargs, w = operands vargs w in
     w, Pcall (Some l, t, f, args, vargs)
   | `alloc (x, n) -> go x @@ fun w -> w, Palloc (Some l, n)
   | `load (x, t, a) -> go x @@ fun w ->
-    let a, w = operand a w l in
+    let a, w = operand a w in
     w, Pload (Some l, t, a)
   | `store _ -> acc
   | `vaarg (x, _, y) ->
     let w, xs = acc in
-    let w = Worklist.remove w x in
-    let w = Worklist.remove w y in
+    let w = Set.remove w x in
+    let w = Set.remove w y in
     w, xs
   | `vastart x ->
     let w, xs = acc in
-    Worklist.remove w x, xs
+    Set.remove w x, xs
 
 (* Kill the variables that appear in the arguments of the block.
    This is the point where we can no longer represent their data
@@ -217,7 +199,7 @@ let accum t acc i =
 *)
 let killed blk w =
   Blk.args blk |> Seq.map ~f:fst |>
-  Seq.fold ~init:w ~f:Worklist.remove
+  Seq.fold ~init:w ~f:Set.remove
 
 let different_insn l i = Label.(l <> Insn.label i)
 
@@ -247,12 +229,12 @@ let initq blk l w =
 
 (* Traverse the data dependencies. *)
 let traverse t input blk l w =
-  if not @@ Worklist.is_empty w then
+  if not @@ Set.is_empty w then
     let q = Stack.singleton @@ initq blk l w in
     while not @@ Stack.is_empty q do
       let blk, l, w, bs, xs = Stack.pop_exn q in
       let w, xs = insns t blk l w xs in
-      if not @@ Worklist.is_empty w then
+      if not @@ Set.is_empty w then
         let bs = Set.add bs @@ Blk.label blk in
         nexts input blk bs |> Seq.iter ~f:(fun blk ->
             Stack.push q (blk, Blk.label blk, w, bs, xs))
@@ -270,23 +252,23 @@ let ctrl t eg blk =
   match Blk.ctrl blk with
   | `hlt -> ()
   | `jmp d -> go @@ fun () ->
-    let d, w = dst d Worklist.empty l in
+    let d, w = dst d Var.Set.empty in
     w, Ejmp d
   | `br (c, y, n) -> go @@ fun () ->
-    let c, w = Exp.Pvar c, Worklist.singleton c l in
-    let y, w = dst y w l in
-    let n, w = dst n w l in
+    let c, w = Exp.Pvar c, Var.Set.singleton c in
+    let y, w = dst y w in
+    let n, w = dst n w in
     w, Ebr (c, y, n)
   | `ret None -> ()
   | `ret (Some x) -> go @@ fun () ->
-    let x, w = operand x Worklist.empty l in
+    let x, w = operand x Var.Set.empty in
     w, Eret x
   | `sw (t, v, d, tbl) -> go @@ fun () ->
     let v, w = match v with
-      | `var x -> Exp.Pvar x, Worklist.singleton x l
-      | `sym (s, n) -> Exp.Psym (s, n), Worklist.empty in
-    let d, w = local d w l in
-    let tbl, w = table tbl w l in
+      | `var x -> Exp.Pvar x, Var.Set.singleton x
+      | `sym (s, n) -> Exp.Psym (s, n), Var.Set.empty in
+    let d, w = local d w in
+    let tbl, w = table tbl w in
     w, Esw (t, v, d, tbl)
 
 let insn t eg i blk =
@@ -294,34 +276,34 @@ let insn t eg i blk =
   let go = run t eg blk l in
   match Insn.op i with
   | `bop (x, o, a, b) -> go @@ fun () ->
-    let a, w = operand a Worklist.empty l in
-    let b, w = operand b w l in
+    let a, w = operand a Var.Set.empty in
+    let b, w = operand b w in
     w, Eset (x, Pbinop (Some l, o, a, b))
   | `uop (x, o, a) -> go @@ fun () ->
-    let a, w = operand a Worklist.empty l in
+    let a, w = operand a Var.Set.empty in
     w, Eset (x, Punop (Some l, o, a))
   | `sel (x, t, c, y, n) -> go @@ fun () ->
-    let y, w = operand y (Worklist.singleton c l) l in
-    let n, w = operand n w l in
+    let y, w = operand y @@ Var.Set.singleton c in
+    let n, w = operand n w in
     w, Eset (x, Psel (Some l, t, Pvar c, y, n))
   | `call (Some (x, t), f, args, vargs) -> go @@ fun () ->
-    let f, w = global f Worklist.empty l in
-    let args, w = operands args w l in
-    let vargs, w = operands vargs w l in
+    let f, w = global f Var.Set.empty in
+    let args, w = operands args w in
+    let vargs, w = operands vargs w in
     w, Eset (x, Pcall (Some l, t, f, args, vargs))
   | `alloc (x, n) -> go @@ fun () ->
-    Worklist.empty, Eset (x, Palloc (Some l, n))
+    Var.Set.empty, Eset (x, Palloc (Some l, n))
   | `load (x, t, a) -> go @@ fun () ->
-    let a, w = operand a Worklist.empty l in
+    let a, w = operand a Var.Set.empty in
     w, Eset (x, Pload (Some l, t, a))
   | `call (None, f, args, vargs) -> go @@ fun () ->
-    let f, w = global f Worklist.empty l in
-    let args, w = operands args w l in
-    let vargs, w = operands vargs w l in
+    let f, w = global f Var.Set.empty in
+    let args, w = operands args w in
+    let vargs, w = operands vargs w in
     w, Ecall (f, args, vargs)
   | `store (t, v, a) -> go @@ fun () ->
-    let v, w = operand v Worklist.empty l in
-    let a, w = operand a w l in
+    let v, w = operand v Var.Set.empty in
+    let a, w = operand a w in
     w, Estore (t, v, a)
   | `vaarg _ | `vastart _ -> ()
 
