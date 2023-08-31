adfbe3b52567d76cb580d7ca652f3b501bff21fc
bmourad01 <Benjamin_Mourad@student.uml.edu>
2023-08-31 08:42:31 -0400

    Repeat `Remove_dead_vars` and `Simplify_cfg` before `Peephole`

--- src/bin/cgen_main.ml
+++ src/bin/cgen_main.ml
@@ -38,6 +38,8 @@ let comp filename =
   let*? tenv = Typecheck.run m ~target in
   let*? m = Virtual.Module.map_funs_err m ~f:Passes.Ssa.run in
   Format.printf "%a\n%!" Virtual.Module.pp m;
+  let*? m = Virtual.Module.map_funs_err m ~f:Passes.Remove_dead_vars.run in
+  let*? m = Virtual.Module.map_funs_err m ~f:Passes.Simplify_cfg.run in
   let* m = Context.Virtual.Module.map_funs m ~f:(Passes.Peephole.run tenv) in
   let*? m = Virtual.Module.map_funs_err m ~f:Passes.Remove_dead_vars.run in
   let m = Virtual.Module.map_funs m ~f:Passes.Remove_disjoint_blks.run in
