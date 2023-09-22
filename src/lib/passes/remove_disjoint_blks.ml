5a9a861584f349b0054ef5c7df42aa2db889be10
bmourad01 <Benjamin_Mourad@student.uml.edu>
2023-09-22 17:35:24 -0400

    Use `with_return`

--- src/lib/passes/remove_disjoint_blks.ml
+++ src/lib/passes/remove_disjoint_blks.ml
@@ -3,19 +3,14 @@ open Graphlib.Std
 open Regular.Std
 open Virtual
 
-exception Done of Label.Set.t
-
-let dfs cfg start =
+let reachable fn = with_return @@ fun {return} ->
+  let cfg = Cfg.create fn in
+  let start = Func.entry fn in
   Graphlib.depth_first_search (module Cfg) cfg ~start
     ~init:Label.Set.empty
     ~start_tree:(fun n s ->
-        if Label.(n = start) then s else raise @@ Done s)
+        if Label.(n = start) then s else return s)
     ~enter_node:(fun _ n s -> Set.add s n)
-
-let reachable fn =
-  let cfg = Cfg.create fn in
-  let start = Func.entry fn in
-  try dfs cfg start with Done s -> s
 
 let run fn =
   Func.blks fn |> Seq.map ~f:Blk.label |>
