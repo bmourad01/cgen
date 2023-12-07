255084b99fa4ca80dfa1c7ff15d46097ace08896
bmourad01 <Benjamin_Mourad@student.uml.edu>
2023-12-07 18:42:49 -0500

    Docstring

--- src/lib/passes/lower_abi.mli
+++ src/lib/passes/lower_abi.mli
@@ -1,3 +1,10 @@
+(** This pass lowers a Virtual function into a Virtual ABI
+    function, where parameter passing is desugared and made
+    to comform to a specific ABI.
+
+    The function is assumed to be in SSA form.
+*)
+
 open Virtual
 
 val run : Typecheck.env -> func -> Abi.func Context.t
