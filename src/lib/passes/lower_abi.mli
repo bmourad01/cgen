2ee0f95c9431c3083e373fc18bc29370b11a09ef
bmourad01 <Benjamin_Mourad@student.uml.edu>
2024-03-16 17:58:09 -0400

    Clarify docstring for `Lower_abi`

--- src/lib/passes/lower_abi.mli
+++ src/lib/passes/lower_abi.mli
@@ -2,7 +2,9 @@
     function, where parameter passing is desugared and made
     to comform to a specific ABI.
 
-    The function is assumed to be in SSA form.
+    The function is assumed to be in SSA form, and the transformed
+    function is expected to preserve it, modulo the presence of
+    explicit registers.
 *)
 
 val run : Typecheck.env -> Virtual.func -> Virtual.Abi.func Context.t
