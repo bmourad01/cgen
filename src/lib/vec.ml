b7e07c0c7d736f6037c181ed20eaf36d9750d005
bmourad01 <Benjamin_Mourad@student.uml.edu>
2024-05-04 17:25:01 -0400

    Amortize the cost of future appends/pushes for `Vec.append`

--- src/lib/vec.ml
+++ src/lib/vec.ml
@@ -36,10 +36,14 @@ let append t1 t2 =
     let l2 = t2.length in
     let new_length = l1 + l2 in
     let dst = if t1.capacity < new_length then
-        let dst = Oa.create ~len:new_length in
+        (* Try to ensure that future additions won't immediately
+           trigger a resizing of the backing array. Hopefully
+           this doesn't end up wasting too much memory. *)
+        let new_capacity = new_length * growth_factor in
+        let dst = Oa.create ~len:new_capacity in
         Oa.unsafe_blit ~src:t1.data ~src_pos:0 ~dst ~dst_pos:0 ~len:l1;
         t1.data <- dst;
-        t1.capacity <- new_length;
+        t1.capacity <- new_capacity;
         dst
       else t1.data in
     Oa.unsafe_blit ~src:t2.data ~src_pos:0 ~dst ~dst_pos:l1 ~len:l2;
