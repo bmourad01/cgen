517e89720c924455dca5689bb179687f1b3012d5
bmourad01 <Benjamin_Mourad@student.uml.edu>
2024-05-06 12:56:51 -0400

    Catch some unhandled errors in `Resolve_constant_blk_args`
    These would only happen for ill-formed programs, but we want a nicer
    error message.

--- src/lib/passes/resolve_constant_blk_args.ml
+++ src/lib/passes/resolve_constant_blk_args.ml
@@ -3,6 +3,10 @@ open Regular.Std
 open Graphlib.Std
 open Virtual
 
+exception Missing of Label.t
+exception Redef of Var.t * Label.t
+exception Arity of Label.t * Label.t * int * int
+
 let collect_copies blks fn dom =
   let t = Var.Table.create () in
   let q = Stack.singleton @@ Func.entry fn in
@@ -14,7 +18,10 @@ let collect_copies blks fn dom =
                   | `var z -> Hashtbl.find_or_add t z ~default:(const y)
                   | _ -> y in
                 (* Should never fail. *)
-                Hashtbl.add_exn t ~key:x ~data
+                begin match Hashtbl.add t ~key:x ~data with
+                  | `Duplicate -> raise_notrace @@ Redef (x, Blk.label b)
+                  | `Ok -> ()
+                end
               | _ -> ()));
       Tree.children dom l |> Seq.iter ~f:(Stack.push q));
   t
@@ -48,34 +55,35 @@ let join_value cpy x y = match x, y with
   | One a, One b when equal_operand a b -> x
   | One _, One _ -> Many
 
-let local cpy blks s : local -> state = function
-  | `label (_, []) -> s
-  | `label (l, vs) ->
-    Label.Tree.find blks l |>
-    Option.value_map ~default:s ~f:(fun b ->
-        let args = Seq.to_list @@ Blk.args b in
-        List.zip_exn args vs |>
-        List.fold ~init:s ~f:(fun s (x, v) ->
-            Map.update s x ~f:(function
-                | Some v' -> join_value cpy v' @@ One v
-                | None -> One v)))
+let local cpy blks s bl : local -> state = function
+  | `label (l, vs) -> match Label.Tree.find blks l with
+    | None -> raise_notrace @@ Missing l
+    | Some b ->
+      let args = Seq.to_list @@ Blk.args b in
+      match List.zip args vs with
+      | Unequal_lengths ->
+        raise_notrace @@ Arity (bl, l, List.length vs, List.length args)
+      | Ok xs -> List.fold xs ~init:s ~f:(fun s (x, v) ->
+          Map.update s x ~f:(function
+              | Some v' -> join_value cpy v' @@ One v
+              | None -> One v))
 
-let dst cpy blks s : dst -> state = function
-  | #local as l -> local cpy blks s l
+let dst cpy blks s bl : dst -> state = function
+  | #local as l -> local cpy blks s bl l
   | #global -> s
 
-let transfer cpy blks l s =
-  Label.Tree.find blks l |>
-  Option.value_map ~default:s ~f:(fun b ->
-      match Blk.ctrl b with
-      | `hlt | `ret _ -> s
-      | `jmp d -> dst cpy blks s d
-      | `br (_, y, n) ->
-        dst cpy blks (dst cpy blks s y) n
-      | `sw (_, _, d, tbl) ->
-        let init = local cpy blks s d in
-        Ctrl.Table.enum tbl |> Seq.fold ~init
-          ~f:(fun s (_, l) -> local cpy blks s l))
+let transfer cpy blks l s = match Label.Tree.find blks l with
+  | None when Label.is_pseudo l -> s
+  | None -> raise_notrace @@ Missing l
+  | Some b -> match Blk.ctrl b with
+    | `hlt | `ret _ -> s
+    | `jmp d -> dst cpy blks s l d
+    | `br (_, y, n) ->
+      dst cpy blks (dst cpy blks s l y) l n
+    | `sw (_, _, d, tbl) ->
+      let init = local cpy blks s l d in
+      Ctrl.Table.enum tbl |> Seq.fold ~init
+        ~f:(fun s (_, l') -> local cpy blks s l l')
 
 let merge cpy = Map.merge_skewed ~combine:(fun ~key:_ -> join_value cpy)
 
@@ -94,16 +102,26 @@ let analyze fn =
      since this is a forward-flow analysis. *)
   filter_many @@ Solution.get soln Label.pseudoexit
 
+let error_prefix = "In Resolve_constant_blk_args"
+
+let try_ fn f = try Ok (f ()) with
+  | Missing l ->
+    Or_error.errorf "%s: in function $%s, missing block %a"
+      error_prefix (Func.name fn) Label.pps l
+  | Redef (x, l) ->
+    Or_error.errorf "%s: in function $%s, redefinition of variable %a in block %a"
+      error_prefix (Func.name fn) Var.pps x Label.pps l
+  | Arity (l, l', n, n') ->
+    Or_error.errorf "%s: in function $%s, arity mismatch in edge %a -> %a (%d != %d)"
+      error_prefix (Func.name fn) Label.pps l Label.pps l' n n'
+
 let run fn =
-  if Dict.mem (Func.dict fn) Tags.ssa then
+  if Dict.mem (Func.dict fn) Tags.ssa then try_ fn @@ fun () ->
     let s = analyze fn in
-    let fn =
-      if not @@ Map.is_empty s then Func.map_blks fn ~f:(fun b ->
-          let is, k = Subst_mapper.map_blk s b in
-          Blk.(with_ctrl (with_insns b is) k))
-      else fn in
-    Ok fn
+    if not @@ Map.is_empty s then Func.map_blks fn ~f:(fun b ->
+        let is, k = Subst_mapper.map_blk s b in
+        Blk.(with_ctrl (with_insns b is) k))
+    else fn
   else
-    Or_error.errorf
-      "In Resolve_constant_blk_args: function $%s is not in SSA form"
-      (Func.name fn)
+    Or_error.errorf "%s: function $%s is not in SSA form"
+      error_prefix (Func.name fn)
