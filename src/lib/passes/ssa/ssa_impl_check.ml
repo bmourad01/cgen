efbbc4faebd815db7e3ba0a00d63d460994ab324
bmourad01 <delysin@protonmail.com>
2025-09-28 16:44:57 -0400

    Slight improvement to SSA check message

--- src/lib/passes/ssa/ssa_impl_check.ml
+++ src/lib/passes/ssa/ssa_impl_check.ml
@@ -8,14 +8,16 @@ module Make(M : L) : sig
 end = struct
   open M
 
-  let fail fn = failwithf "$%s violates SSA invariants" (Func.name fn) ()
+  let fail fn msg =
+    failwithf "$%s violates SSA invariants: %s"
+      (Func.name fn) msg ()
 
   let check_dom ?(k = Fn.id) dom fn b b' =
     let l = Blk.label b in
     let l' = Blk.label b' in
     if Label.(l = l') then k ()
     else if not (Semi_nca.Tree.is_descendant_of dom ~parent:l l')
-    then fail fn
+    then fail fn "dominance check failed"
 
   (* The resolver should handle multiple definitions, as well as uses
      with no definitions. Our remaining task is to check that each
@@ -34,5 +36,8 @@ end = struct
                     | `insn (_, b', _,  ord') ->
                       check_dom dom fn b b' ~k:(fun () ->
                           (* Check that `i` is defined before `i'`. *)
-                          if ord >= ord' then fail fn)))))
+                          if ord >= ord' then
+                            fail fn @@ Format.asprintf
+                              "use before definition of %a"
+                              Var.pp x)))))
 end
