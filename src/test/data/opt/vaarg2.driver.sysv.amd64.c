9814437f5f0b780af987d26783b59f9dc993e2db
bmourad01 <delysin@protonmail.com>
2025-11-08 17:55:01 -0500

    progress

---
+++ src/test/data/opt/vaarg2.driver.sysv.amd64.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+
+extern long foo(long i, ...);
+extern char bar(char b, ...); /* NB: bar actually has no named arguments */
+
+struct t {
+  long l1;
+  long l2;
+};
+
+int main() {
+  struct t s = {10, 32};
+  assert(foo(5, s) == 47);
+  assert(!bar(0));
+  assert(bar(1));
+}
