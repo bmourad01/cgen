a735553b7377109aac9c694a1c759db651d1ab8c
bmourad01 <delysin@protonmail.com>
2026-06-29 00:46:30 -0400

    Remove disjoint blocks right after ABI lowering

--- src/lib/cgen/passes/lower_abi.ml
+++ src/lib/cgen/passes/lower_abi.ml
@@ -6,6 +6,8 @@ let run ?(invariants = false) tenv fn =
     let* (module Machine) = Context.machine in
     let module Lower = Machine.Lower_abi(Context) in
     let* fn = Lower.run tenv fn in
+    (* This is needed so that we don't fail the SSA check. *)
+    let fn = Remove_disjoint_blks.run_abi fn in
     (* The SSA tag must be set unconditionally, since the downstream ABI
        passes require it. When `invariants` is disabled we trust that
        `Lower.run` preserved SSA form rather than paying to re-verify it. *)
